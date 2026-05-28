b08bb657ddfc5277bfacb6a2c5afdb6c4310b427
Davi Costa <contato.davikosta@gmail.com>
2026-05-28 14:22:31 -0300

    Refatora código, remove arquivos compilados, testa com sucesso em samples da atividade

--- tipo_lista.c
+++ tipo_lista.c
@@ -5,79 +5,70 @@
 
 typedef struct {
     int chave;
-} TipoItem;
+} tipoItem;
 
 typedef struct {
-    TipoItem Item[MAX];
+    tipoItem item[MAX];
     int tamanho;
-} TipoLista;
+} tipoLista;
 
 int obtemN() {
     int n;
-    //printf("Digite o tamanho da lista:\n");
     scanf("%d", &n);
-    if(n < MAX) {
-        return n;
-    }
+    return n;
     
 }
 
 int obtemP() {
     int p;
-    //printf("Digite a posição que deseja inserir:\n");
     scanf("%d", &p);
     return p;
 }
 
 int obtemX() {
     int x;
-    //printf("Digite o valor a ser inserido:\n");
     scanf("%d", &x);
     return x;
 }
 
-void inicializaLista(TipoLista *lista, int n) {
+void inicializaLista(tipoLista *lista, int n) {
     lista->tamanho = n;
 
-    //printf("Insira a lista de N:\n");
     for (int i = 0; i < lista->tamanho; i++)
     {
-        scanf("%d", &lista->Item[i].chave);
+        scanf("%d", &lista->item[i].chave);
     }
     
 }
 
-void insereLista(TipoLista *lista, int p, int x) {
+void insereLista(tipoLista *lista, int p, int x) {
 
-    if(p <= lista->tamanho && p > 0) {
+    if(lista->tamanho < 100 && p <= lista->tamanho + 1 && p > 0) {
         for(int i = lista->tamanho; i >= p; i--) {
-            lista->Item[i].chave = lista->Item[i-1].chave;
+            lista->item[i].chave = lista->item[i-1].chave;
         }
-        lista->Item[p-1].chave = x;
+        lista->item[p-1].chave = x;
         lista->tamanho++;
 
         for(int i = 0; i < lista->tamanho; i++) {
-            printf("%d\n", lista->Item[i].chave);
+            printf("%d\n", lista->item[i].chave);
         }
     } else {
         printf("Lista esta cheia ou posicao inválida\n");
     }
 }
 
-void exibeLista(TipoLista *lista) {
-    
-}
-
-
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
     
-    TipoLista lista;
+    tipoLista lista;
 
     inicializaLista(&lista, obtemN());
-
-    insereLista(&lista, obtemP(), obtemX());
+    
+	int x = obtemX();
+	int p = obtemP();
+    insereLista(&lista, p, x);
 
     return 0;
 }
