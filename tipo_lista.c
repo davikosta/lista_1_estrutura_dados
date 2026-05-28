663a067d2cc44c660c8a96f03c5e0264990dda1b
Davi Costa <contato.davikosta@gmail.com>
2026-05-28 10:21:14 -0300

    Escreve funções necessárias

--- tipo_lista.c
+++ tipo_lista.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <locale.h>
 
 #define MAX 100
 
@@ -13,33 +14,70 @@ typedef struct {
 
 int obtemN() {
     int n;
-    printf("Insira o tamanho da lista");
+    //printf("Digite o tamanho da lista:\n");
     scanf("%d", &n);
-    return n;
+    if(n < MAX) {
+        return n;
+    }
+    
 }
 
-void inicializaLista(TipoLista *lista) {
-    lista->tamanho = obtemN();
+int obtemP() {
+    int p;
+    //printf("Digite a posição que deseja inserir:\n");
+    scanf("%d", &p);
+    return p;
+}
 
-    printf("Insira a lista de N");
+int obtemX() {
+    int x;
+    //printf("Digite o valor a ser inserido:\n");
+    scanf("%d", &x);
+    return x;
+}
+
+void inicializaLista(TipoLista *lista, int n) {
+    lista->tamanho = n;
+
+    //printf("Insira a lista de N:\n");
     for (int i = 0; i < lista->tamanho; i++)
     {
         scanf("%d", &lista->Item[i].chave);
     }
-    for (int i = 0; i < 3; i++)
-    {
-        printf("%d", lista->Item[i].chave);
+    
+}
+
+void insereLista(TipoLista *lista, int p, int x) {
+
+    if(p <= lista->tamanho && p > 0) {
+        for(int i = lista->tamanho; i >= p; i--) {
+            lista->Item[i].chave = lista->Item[i-1].chave;
+        }
+        lista->Item[p-1].chave = x;
+        lista->tamanho++;
+
+        for(int i = 0; i < lista->tamanho; i++) {
+            printf("%d\n", lista->Item[i].chave);
+        }
+    } else {
+        printf("Lista esta cheia ou posicao inválida\n");
     }
+}
+
+void exibeLista(TipoLista *lista) {
     
 }
 
 
 
 int main() {
+    setlocale(LC_ALL, "Portuguese");
     
     TipoLista lista;
 
-    inicializaLista(&lista);
+    inicializaLista(&lista, obtemN());
+
+    insereLista(&lista, obtemP(), obtemX());
 
     return 0;
 }
